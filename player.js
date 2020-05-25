24a30393b4b9981c0f37890185ee2dd54bb270f5
hebsonaraujo <hebsonaraujo@hotmail.com>
2020-05-24 23:39:15 -0300

    Add files via upload

---
+++ player.js
@@ -0,0 +1,12 @@
+/**
+ * Class que representa o jogador.
+ * @extends Cards
+ */
+class Player extends Cards{
+    constructor() {
+        super()        
+        this.id;
+        this.score = 0;
+        
+    }
+}
